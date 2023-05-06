9461f3f88dd0c5afb16a1d1226c9764e72686f20
Wahyu Setiawan <wahyusetiawanblog9999@gmail.com>
2023-05-06 11:18:52 +0800

    test CI

--- src/Applications/use_case/LikeCommentUseCase.js
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -1,17 +1,18 @@
-class LikeCommentReplyUseCase {
-  constructor({ threadRepository, commentRepository, replyRepository }) {
-    this._threadRepository = threadRepository;
-    this._commentRepository = commentRepository;
-    this._replyRepository = replyRepository;
-  }
+// class LikeCommentReplyUseCase {
+//   constructor({ threadRepository, commentRepository, replyRepository }) {
+//     this._threadRepository = threadRepository;
+//     this._commentRepository = commentRepository;
+//     this._replyRepository = replyRepository;
+//   }
 
-  async execute(owner, useCasePayload) {
-    const postReply = new PostReply(useCasePayload);
-    await this._threadRepository.findThreadById(postReply.thread);
-    await this._commentRepository.findCommentById(postReply.comment);
-    await this._commentRepository.verifyCommentBelongToThread(postReply.comment, postReply.thread);
-    return this._replyRepository.addReply(owner, postReply);
-  }
-}
+//   async execute(owner, useCasePayload) {
+//     const postReply = new PostReply(useCasePayload);
+//     await this._threadRepository.findThreadById(postReply.thread);
+//     await this._commentRepository.findCommentById(postReply.comment);
+//     await this._commentRepository
+// .verifyCommentBelongToThread(postReply.comment, postReply.thread);
+//     return this._replyRepository.addReply(owner, postReply);
+//   }
+// }
 
-module.exports = LikeCommentReplyUseCase;
+// module.exports = LikeCommentReplyUseCase;
