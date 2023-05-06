d79b63598c8a0031f924eb409918499a9239f829
Wahyu Setiawan <wahyusetiawanblog9999@gmail.com>
2023-05-06 16:19:20 +0800

    feat: progress like feature

--- src/Domains/like_comments/LikeCommentRepository.js
+++ src/Domains/like_comments/LikeCommentRepository.js
@@ -1,33 +1,33 @@
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable class-methods-use-this */
-// class LikeCommentRepository {
-//   async addComment(postComment) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+class LikeCommentRepository {
+  async addComment(postComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async findCommentById(id) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+  async findCommentById(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async getCommentByThreadId(thread) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+  async getCommentByThreadId(thread) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async verifyCommentOwner(id, owner) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+  async verifyCommentOwner(id, owner) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async verifyCommentDeletion(id) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+  async verifyCommentDeletion(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async verifyCommentBelongToThread(id, thread) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
+  async verifyCommentBelongToThread(id, thread) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 
-//   async deleteComment(id) {
-//     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-//   }
-// }
+  async deleteComment(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
 
-// module.exports = LikeCommentRepository;
+module.exports = LikeCommentRepository;
