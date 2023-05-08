f4492f0341c986584f09b7ca44df4abb8db1fbcd
Wahyu Setiawan <wahyusetiawanblog9999@gmail.com>
2023-05-08 16:52:24 +0800

    Feat: feature like comment updated

---
+++ src/Applications/use_case/LikeCommentUseCase.js
@@ -0,0 +1,50 @@
+/* eslint-disable class-methods-use-this */
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+
+class LikeCommentUseCase {
+  constructor({ threadRepository, commentRepository, likeCommentRepository }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+    this._likeCommentRepository = likeCommentRepository;
+  }
+
+  async execute(owner, useCasePayload) {
+    this._validatePayload(useCasePayload);
+    const { thread, comment } = useCasePayload;
+
+    await this._threadRepository.findThreadById(thread);
+
+    const checkComment = await this._commentRepository.findCommentById(comment);
+
+    // verify comment belongs to correct thread
+    if (checkComment.thread !== thread) {
+      throw new NotFoundError('komentar tidak ditemukan pada thread yang dimaksud');
+    }
+
+    // verify if comment is deleted
+    if (checkComment.is_delete) {
+      throw new NotFoundError('komentar tidak ditemukan');
+    }
+
+    const checkLikeComment = await this._likeCommentRepository.verifyLikedComment(owner, comment);
+
+    if (checkLikeComment.length > 0) {
+      await this._likeCommentRepository.unlikeComment(owner, comment);
+    } else {
+      await this._likeCommentRepository.likeComment(owner, comment);
+    }
+  }
+
+  _validatePayload(payload) {
+    const { thread, comment } = payload;
+    if (!thread || !comment) {
+      throw new Error('LIKE_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof thread !== 'string' || typeof comment !== 'string') {
+      throw new Error('LIKE_COMMENT.NOT_MEET_DATA_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = LikeCommentUseCase;
